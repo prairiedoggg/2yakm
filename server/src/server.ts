e8c66c77ba1917446ec634eec1981c549d4251cd
civica23@gmail.com <civica23@gmail.com>
2024-07-16 12:30:39 +0900

    feat: PostgreSQL SSL 보안 적용

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import specs from './swagger';
 import pg from 'pg';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,13 +13,29 @@ const port = process.env.PORT || 3000;
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
+// ESM에서는 __dirname을 사용할 수 없어서 만들어줘야함
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const { Pool } = pg;
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
-  port: Number(process.env.DB_PORT)
+  port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+    ca: fs
+      .readFileSync(path.join(__dirname, '../certs/server-ca.pem'))
+      .toString(),
+    key: fs
+      .readFileSync(path.join(__dirname, '../certs/client-key.pem'))
+      .toString(),
+    cert: fs
+      .readFileSync(path.join(__dirname, '../certs/client-cert.pem'))
+      .toString()
+  }
 });
 
 pool.connect((err: any) => {
