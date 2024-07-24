c69e8a028beaabcaf369a99cb5c17d0887276afb
civica23@gmail.com <civica23@gmail.com>
2024-07-25 04:58:35 +0900

    ✨ Feat: pill entity 추가

---
+++ server/src/entity/pill.ts
@@ -0,0 +1,18 @@
+export interface Pill {
+  id: number;
+  name: string;
+  engName: string;
+  companyName: string;
+  companyEngName: string;
+  ingredientName: string;
+  ingredientEngName: string;
+  type: string;
+  shape: string;
+  efficacy: string;
+  dosage: string;
+  caution: string;
+  storageMethod: string;
+  expdate: string;
+  source: string;
+  imgUrl: string;
+}
