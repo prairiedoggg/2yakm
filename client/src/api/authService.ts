6fb77da9fa869c60a2a3051877f9c5dc843db8c5
seona <osa746@gmail.com>
2024-07-26 02:25:03 +0900

    🛠️: 비밀번호 변경 콜백 인자 추가

--- client/src/api/authService.ts
+++ client/src/api/authService.ts
@@ -2,16 +2,7 @@ import { post } from './api';
 import useUserStore from '../store/user';
 import Cookies from 'js-cookie';
 
-export const changePassword = async (email: string, oldPassword: string, newPassword: string, callback?:()=>void) => {
-  try {
-    const data = await post('/api/auth/change-password', { email: email, oldPassword:oldPassword, newPassword:newPassword });
-    if (callback) {
-      callback();
-  }
-  } catch (error) {
-    console.error('Login failed', error);
-  }
-};
+
 
 export const login = async (email: string, password: string) => {
   try {
@@ -29,10 +20,9 @@ export const logout = async (callback?:()=>void) => {
     Cookies.remove('jwt');
     Cookies.remove('token');
     Cookies.remove('refreshToken');
-    if (callback) {
-        callback();
-    }
 
+    if (callback) 
+        callback();
   } catch (error) {
     console.error('Logout failed', error);
   }
@@ -56,13 +46,21 @@ export const loginForGoogle = async (code:string) => {
     }
 };  
 
+export const changePassword = async (email: string, oldPassword: string, newPassword: string, callback?:(arg0:any)=>void) => {
+  try {
+    const data = await post('/api/auth/change-password', { email: email, oldPassword:oldPassword, newPassword:newPassword });
+    if (callback) {
+      callback(data);
+  }
+  } catch (error) {
+    console.error('Login failed', error);
+  }
+};
+
 const storeLoginData = (data:any)=>{
-
-    if(data.user){
+    if(data.user)
         useUserStore.getState().setUser(data.user);
-    }
 
-    if (data.token) {
+    if (data.token) 
         Cookies.set('jwt', `${data.token}`, { path: '/' });
-    }
 }
