5e52f7a51e24418bf2e18f7f73f9ba2fb3d0508e
Wonsik Woo <wonsikwoo@MacBook-Pro.local>
2024-07-25 14:49:30 +0900

    🎨 Style generics

--- server/src/controllers/pillController.ts
+++ server/src/controllers/pillController.ts
@@ -65,11 +65,28 @@ export const deletePillHandler = async (req: Request, res: Response, next: NextF
   }
 };
 
-export const searchPillsbyNameHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+interface QueryParams {
+  name: string;
+  limit?: string;
+  offset?: string;
+  sortedBy?: string;
+  order?: "ASC" | "DESC";
+}
+
+interface RequestParams {
+  id: string;
+}
+
+export const searchPillsbyNameHandler = async (
+  req: Request<unknown, unknown, RequestParams, QueryParams>,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const name = req.query.name as string;
+  const offset = parseInt(req.query.offset ?? '0', 10);
+  const limit = parseInt(req.query.limit ?? '10', 10);
+
   try {
-    const name = req.query.name as string;
-    const offset = parseInt(req.query.offset as string, 10) || 0;
-    const limit = parseInt(req.query.limit as string, 10) || 10;
     const pills = await searchPillsbyName(name, limit, offset);
     res.status(200).json(pills);
   } catch (error) {
@@ -77,11 +94,16 @@ export const searchPillsbyNameHandler = async (req: Request, res: Response, next
   }
 };
 
-export const searchPillsbyEngNameHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+export const searchPillsbyEngNameHandler = async (
+  req: Request<unknown, unknown, RequestParams, QueryParams>,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const name = req.query.name as string;
+  const offset = parseInt(req.query.offset ?? '0', 10);
+  const limit = parseInt(req.query.limit ?? '10', 10);
+
   try {
-    const name = req.query.name as string;
-    const offset = parseInt(req.query.offset as string, 10) || 0;
-    const limit = parseInt(req.query.limit as string, 10) || 10;
     const pills = await searchPillsbyEngName(name, limit, offset);
     res.status(200).json(pills);
   } catch (error) {
@@ -89,12 +111,21 @@ export const searchPillsbyEngNameHandler = async (req: Request, res: Response, n
   }
 };
 
+interface EfficacyQueryParams {
+  efficacy: string;
+  limit?: string;
+  offset?: string;
+}
 
-export const searchPillsbyEfficacyHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+export const searchPillsbyEfficacyHandler = async (
+  req: Request<unknown, unknown, unknown, EfficacyQueryParams>,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const efficacy = req.query.efficacy as string;
-    const offset = parseInt(req.query.offset as string, 10) || 0;
-    const limit = parseInt(req.query.limit as string, 10) || 10;
+    const offset = parseInt(req.query.offset ?? '0', 10);
+    const limit = parseInt(req.query.limit ?? '10', 10);
     const pills = await searchPillsbyEfficacy(efficacy, limit, offset);
     res.status(200).json(pills);
   } catch (error) {
@@ -102,21 +133,29 @@ export const searchPillsbyEfficacyHandler = async (req: Request, res: Response, 
   }
 };
 
-export const searchPillsByImageHandler = async (req: Request, res: Response, next: NextFunction) => {
+interface ImageQueryParams {
+  limit?: string;
+  offset?: string;
+}
+
+export const searchPillsByImageHandler = async (
+  req: Request<unknown, unknown, unknown, ImageQueryParams>,
+  res: Response,
+  next: NextFunction
+)=> {
   try {
     if (!req.file) {
       return res.status(400).json({ message: 'No file uploaded' });
     }
     const imageBuffer = req.file.buffer;
-    const limit = parseInt(req.query.limit as string, 10) || 10;
-    const offset = parseInt(req.query.offset as string, 10) || 0;
+    const limit = parseInt(req.query.limit ?? '10', 10);
+    const offset = parseInt(req.query.offset ?? '0', 10);
     const result = await searchPillsByImage(imageBuffer, limit, offset);
     res.status(200).json(result);
   } catch (error) {
     next(error);
   }
 };
-
 
 export const getPillFavoriteCount = async (
    req: Request<{ id: string }, unknown, unknown, unknown>,
