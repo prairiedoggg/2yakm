474d3998afaf97452218fc208e891f65870700e1
서승범 <phi0724@naver.com>
2024-08-06 09:32:37 +0000

    Chore: 빌드 권한

--- start.sh
+++ start.sh
@@ -1,8 +1,15 @@
+   #!/bin/bash
+   set -e
+   nginx
+   # 환경 변수 출력 (디버깅용)
+   env
 
-nginx
+   # 현재 디렉토리 및 파일 구조 출력 (디버깅용)
+   pwd
+   ls -R /chicken_pharm
 
-cd /chicken_pharm/server && npm start &
+   # 클라이언트 시작 (5173 포트 사용)
+   cd /chicken_pharm/client && npm run preview -- --port 5173 &
 
-cd /chicken_pharm/client && npm run preview
-
-wait
+   # 서버 시작 (3000 포트 사용)
+   cd /chicken_pharm/server && NODE_ENV=production node dist/server.js
