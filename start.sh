351f6591974fd4304e27f9c00006de459455a847
서승범 <phi0724@naver.com>
2024-08-06 09:41:23 +0000

    🚀 Chore:start.sh 변경~

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
    #!/bin/bash
    set -e
    nginx
-   # 환경 변수 출력 (디버깅용)
-   env
-
-   # 현재 디렉토리 및 파일 구조 출력 (디버깅용)
-   pwd
-   ls -R /chicken_pharm
 
    # 클라이언트 시작 (5173 포트 사용)
    cd /chicken_pharm/client && npm run preview -- --port 5173 &
