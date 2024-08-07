8f7e1ab310c227f47d601eb88b5d6b33b191f10c
[AI10] 임지영 <dlawldud021@naver.com>
2024-08-07 21:32:08 +0900

    ✨ Feat: 로딩, 검색결과값 없음 컴포넌트

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,41 +1,81 @@
-import styled from 'styled-components';
-import { Icon } from '@iconify-icon/react';
+import styled, { keyframes } from 'styled-components';
 
-const Loading = ({}: {}) => {
+const Loading = () => {
   return (
-    <Overlay>
-      <Icon
-        icon='eos-icons:bubble-loading'
-        width='4rem'
-        height='4rem'
-        style={{ color: 'white' }}
-      />
-    </Overlay>
+    <Container>
+      <Overlay>
+        <AnimationContainer>
+          <Image src='/img/loading.png' />
+          <LoadingBottom>&nbsp;</LoadingBottom>
+        </AnimationContainer>
+      </Overlay>
+      <Text>
+        정보를 불러오는 중이에요 <br /> 잠시만 기다려 주세요
+      </Text>
+    </Container>
   );
 };
 
+const fadeInAndOut = keyframes`
+  0% {
+    opacity: 0;
+    height: 0;
+  }
+  50% {
+    opacity: 1;
+    height: 150px;
+  }
+  100% {
+    opacity: 0;
+    height: 0;
+  }
+`;
+
+const Container = styled.div``;
+
 const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
+  position: absolute;
   width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  opacity: 0;
-  animation: fadeIn 0.3s ease-in forwards;
+  height: 80%;
   z-index: 999;
   display: flex;
   justify-content: center;
   align-items: center;
+`;
 
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
+const AnimationContainer = styled.div`
+  position: relative;
+`;
+
+const Image = styled.img`
+  width: 150px;
+  position: relative;
+  z-index: 10;
+`;
+
+const LoadingBottom = styled.div`
+  background-color: #72bf44;
+  width: 143.5px;
+  height: 110px;
+  position: relative;
+  left: 3px;
+  top: -30px;
+  z-index: 5;
+  border-bottom-left-radius: 44%;
+  border-bottom-right-radius: 44%;
+
+  opacity: 0;
+  animation: ${fadeInAndOut} 2s infinite;
+`;
+
+const Text = styled.div`
+  text-align: center;
+  position: absolute;
+  bottom: 32%;
+  left: 26%;
+  font-size: 14pt;
+  color: #6f6f6f;
+  line-height: 25px;
 `;
 
 export default Loading;
