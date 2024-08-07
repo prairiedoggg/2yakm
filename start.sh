7fce2a42c5c3082867381a97c4c2bf130ba0b878
서승범 <phi0724@naver.com>
2024-08-07 13:14:27 +0000

    ☔️ Test: start.sh 변경

--- start.sh
+++ start.sh
@@ -1,15 +1,9 @@
    #!/bin/bash
    set -e
    nginx
-
-
-   # 인증서 파일 확인
-   echo "Checking SSL certificate files..."
-   ls -l /chicken_pharm/server/certs
  
   # 클라이언트 시작 (5173 포트 사용)
    cd /chicken_pharm/client && npm run preview -- --port 5173 &
 
    # 서버 시작 (3000 포트 사용)
-   cd /chicken_pharm/server/src
-   NODE_ENV=production node dist/server.js
+    cd /chicken_pharm/server && npm start
