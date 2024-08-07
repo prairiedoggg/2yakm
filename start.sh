e58c6b8807ac20a6ad8db83a7a537a95f7f46e48
[AI10]서승범 <phi0724@naver.com>
2024-08-08 08:55:04 +0900

    Test:run

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
    nginx
 
 # 클라이언트 시작 (5173 포트 사용)
-   cd /chicken_pharm/client && npm run preview -- --port 5173 &
+   cd /chicken_pharm/client && npm run dev -- --port 5173 &
 
    # 서버 시작 (3000 포트 사용)
    cd /chicken_pharm/server && npm start
