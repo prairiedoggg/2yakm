510eb76cfb1fe93ab0c1c54bd640ef57fe66e764
서승범 <phi0724@naver.com>
2024-08-07 01:58:49 +0000

    🚀 Test:start.sh 로그 수정

--- start.sh
+++ start.sh
@@ -2,7 +2,12 @@
    set -e
    nginx
 
-   # 클라이언트 시작 (5173 포트 사용)
+
+   # 인증서 파일 확인
+   echo "Checking SSL certificate files..."
+   ls -l /chicken_pharm/server/certs
+ 
+  # 클라이언트 시작 (5173 포트 사용)
    cd /chicken_pharm/client && npm run preview -- --port 5173 &
 
    # 서버 시작 (3000 포트 사용)
