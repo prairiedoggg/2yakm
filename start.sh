9b513064138c98d75eece7e9443d17c5ab7903fa
[AI10]서승범 <phi0724@naver.com>
2024-08-08 05:25:18 +0900

    ☔️ Test:클라이언트 동적 서빙

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-   #!/bin/bash
+#!/bin/bash
    set -e
    nginx
- 
-  # 클라이언트 시작 (5173 포트 사용)
-   #cd /chicken_pharm/client && npm run preview -- --port 5173 &
+
+# 클라이언트 시작 (5173 포트 사용)
+   cd /chicken_pharm/client && npm run preview -- --port 5173 &
 
    # 서버 시작 (3000 포트 사용)
-   cd /chicken_pharm/server && node dist/server.js &
+   cd /chicken_pharm/server && npm start
