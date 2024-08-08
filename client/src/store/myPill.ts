d8a557043bec7820b4962640f7e8f666444be8ea
seona <osa746@gmail.com>
2024-08-09 04:03:03 +0900

    🎨 Style : 알람및내약 정보 스토리지 저장/로그아웃시 데이터 클리어

--- client/src/store/myPill.ts
+++ client/src/store/myPill.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 export interface Pill {
   pillid: string;
@@ -13,33 +14,47 @@ interface MyPillState {
   addPill: (pill: Pill) => void;
   addPills: (pills: Pill[]) => void;
   deletePill: (id: string) => void;
+  clearAllPills: () => void;
+
 }
 
-export const useMyPillStore = create<MyPillState>((set) => ({
-  pills: [],
-  setPills: (pills) => set({ pills }),
-
-  addPill: (pill) => set((state) => {
-    const existingPillIds = new Set(state.pills.map(p => p.pillid));
-    if (!existingPillIds.has(pill.pillid)) {
-      return {
-        pills: [...state.pills, pill],
-      };
+export const useMyPillStore = create(
+  persist<MyPillState>(
+    (set) => ({
+      pills: [],
+      setPills: (pills) => set({ pills }),
+    
+      addPill: (pill) => set((state) => {
+        const existingPillIds = new Set(state.pills.map(p => p.pillid));
+        if (!existingPillIds.has(pill.pillid)) {
+          return {
+            pills: [...state.pills, pill],
+          };
+        }
+        return state;
+      }),
+    
+      addPills: (pills) => set((state) => {
+        const existingPillIds = new Set(state.pills.map(pill => pill.pillid));
+        const newPills = pills.filter(pill => !existingPillIds.has(pill.pillid));
+    
+        return {
+          pills: [...state.pills, ...newPills],
+        };
+      }),
+      
+      deletePill: (id: string) =>
+      set((state) => ({
+        pills: state.pills.filter((pill) => pill.pillid !== id)
+      })),
+    
+      clearAllPills: () => {
+        set({ 
+          pills:[],
+         });
+      }
+    }),     {
+      name: 'my-pill-storage'
     }
-    return state;
-  }),
-
-  addPills: (pills) => set((state) => {
-    const existingPillIds = new Set(state.pills.map(pill => pill.pillid));
-    const newPills = pills.filter(pill => !existingPillIds.has(pill.pillid));
-
-    return {
-      pills: [...state.pills, ...newPills],
-    };
-  }),
-  
-  deletePill: (id: string) =>
-  set((state) => ({
-    pills: state.pills.filter((pill) => pill.pillid !== id)
-  })),
-}));
+  )
+);
