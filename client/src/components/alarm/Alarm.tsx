7c0ba716baa8144b76c4b89bc2b0d5af46322d58
seonokmin <dnsmsdkdl@gmail.com>
2024-08-08 20:11:03 +0900

    🚑 Fix : 알람 토스트 에러 수정

--- client/src/components/alarm/Alarm.tsx
+++ client/src/components/alarm/Alarm.tsx
@@ -1,11 +1,17 @@
-import { useEffect } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useAlarmStore } from '../../store/alarm';
 import Nav from '../Nav';
+import Toast from '../Toast';
 import AlarmPage from './AlarmPage';
 import AlarmSettings from './AlarmSettings';
 
+export interface AlarmProps {
+  setShowToast: Dispatch<SetStateAction<string>>;
+}
+
 const Alarm = () => {
+  const [showToast, setShowToast] = useState<string>('');
   const { currentPage, setCurrentPage } = useAlarmStore();
 
   useEffect(() => {
@@ -15,15 +21,16 @@ const Alarm = () => {
   const renderContent = () => {
     switch (currentPage) {
       case 'settings':
-        return <AlarmSettings />;
+        return <AlarmSettings setShowToast={setShowToast} />;
       default:
-        return <AlarmPage />;
+        return <AlarmPage setShowToast={setShowToast} />;
     }
   };
 
   return (
     <PageContainer>
       {renderContent()}
+      {showToast && <Toast onEnd={() => setShowToast('')}>{showToast}</Toast>}
       <Nav />
     </PageContainer>
   );
